993fbe8239a6c809e71e6fefb08a88d746e66868
elchinma <elchinma@code.edu.az>
2022-11-23 01:19:47 +0400

    p005

---
+++ Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p005
+{
+    internal class Admin
+    {
+        public bool IsSuperAdmin;
+        public string Section;
+
+        public Admin(bool issuperadmin, string section)
+        {
+            IsSuperAdmin = issuperadmin;
+            Section = section;
+        }
+        public void ShowAdminAndSection()
+        {
+            Console.WriteLine($"IsSuperAdmin: {IsSuperAdmin}, Section: {Section}");
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p005
+{
+    internal class User
+    {
+        public string Username;
+        public string Password;
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+        public void ShowUserandPass()
+        {
+            Console.WriteLine($"Username: {Username}, Password: {Password}");
+        }
+    
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
